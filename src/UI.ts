a44ae3859c79612436eab0fb3feb52dcef464042
Ethan <ethpsa09@gmail.com>
2024-08-24 16:18:19 +0800

    fix gamepad emote menus fdsuibuyhsgfvuiysvbuisdbiusd

--- src/UI.ts
+++ src/UI.ts
@@ -1,4 +1,4 @@
-import { Modify, JsonUIElement, FontType } from 'jsonui-scripting';
+import { Modify, JsonUIElement, FontType, ButtonMapping } from 'jsonui-scripting';
 
 // Cooldown UI Changes
 Modify.HudScreen('hud_title_text/subtitle_frame/subtitle').setProperty({
@@ -19,6 +19,10 @@ Modify.PersonaCommon('emote_image/empty').setProperty({ size: ["18.85px", "18.85
 Modify.PersonaCommon('emote_image/emote_preview').setProperty({ size: ["18.85px", "18.85px"]})
 Modify.PersonaCommon('emote_wheel_panel/emote_wheel_content_panel').setProperty({ size: [125, 75] })
 
+Modify.PersonaCommon('emote_selection_wheel').setProperty({
+    inner_radius: 0.0,
+    outer_radius: 0.0,
+})
 Modify.PersonaCommon('emote_selection_wheel/emote_slot_0_content').setProperty({
     $wheel_state_texture: "textures/ui/gamemode_switcher_0",
     $emote_touch_label_offset: [0, "50%y + 50% + 3px"],
@@ -41,16 +45,21 @@ Modify.PersonaCommon('emote_selection_wheel/emote_slot_3_content').setProperty({
 })
 Modify.PersonaCommon('emote_wheel_panel/emote_name').setProperty({ font_type: FontType.Default, font_scale_factor: 1.0, size: [125, 32], offset: [0, "-16px"], max_size: [125, 32 ]})
 
-Modify.PersonaCommon('gamepad_hotkey_helpers/quick_select_gamepad_helpers/gamepad_x').setProperty({ layer: 11, offset: [0, 0], size: [8, 8] })
-Modify.PersonaCommon('gamepad_hotkey_helpers/quick_select_gamepad_helpers/gamepad_y').setProperty({ layer: 12, offset: [0, 0], size: [8, 8] })
-Modify.PersonaCommon('gamepad_hotkey_helpers/quick_select_gamepad_helpers/gamepad_a').setProperty({ layer: 13, offset: [0, 0], size: [8, 8] })
-Modify.PersonaCommon('gamepad_hotkey_helpers/quick_select_gamepad_helpers/gamepad_b').setProperty({ layer: 14, offset: [0, 0], size: [8, 8] })
+Modify.PersonaCommon('gamepad_hotkey_helpers/quick_select_gamepad_helpers/gamepad_x').setProperty({ layer: 11, offset: ["-55px", "16px"], size: [8, 8] })
+Modify.PersonaCommon('gamepad_hotkey_helpers/quick_select_gamepad_helpers/gamepad_y').setProperty({ layer: 12, offset: ["-25px", "16px"], size: [8, 8] })
+Modify.PersonaCommon('gamepad_hotkey_helpers/quick_select_gamepad_helpers/gamepad_a').setProperty({ layer: 13, offset: ["7px", "16px"], size: [8, 8] })
+Modify.PersonaCommon('gamepad_hotkey_helpers/quick_select_gamepad_helpers/gamepad_b').setProperty({ layer: 14, offset: ["38px", "16px"], size: [8, 8] })
+
 Modify.PersonaCommon('keyboard_hotkey_helpers/keyboard_1').setProperty({ layer: 11, offset: ["-51px", "21px"], size: [8, 8] })
 Modify.PersonaCommon('keyboard_hotkey_helpers/keyboard_2').setProperty({ layer: 12, offset: ["-20px", "21px"], size: [8, 8] })
 Modify.PersonaCommon('keyboard_hotkey_helpers/keyboard_3').setProperty({ layer: 13, offset: ["11px", "21px"], size: [8, 8] })
 Modify.PersonaCommon('keyboard_hotkey_helpers/keyboard_4').setProperty({ layer: 14, offset: ["42px", "21px"], size: [8, 8] })
 
 Modify.PersonaCommon('emote_image/emote_preview').setProperty({ size: [16, 16] })
+
+Modify.PersonaCommon('gamepad_hotkey_helpers/analog_select_gamepad_helper_panel').setProperty({
+    offset: [0, "26px"]
+})
 
 Modify.EmoteWheelScreen('emote_wheel_screen_content').modifications.controls
     .remove("instruction_padding")
